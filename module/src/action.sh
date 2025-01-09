6f63b301f8989250433d7f0bdfbe1c8139bb57e0
JingMatrix <jingmatrix@gmail.com>
2025-01-09 22:57:07 +0100

    Minor improvements of user experience
    1. Avoid using useless long version names
    2. Name GitHub artifacts differently to avoid confusion
    3. Allow to trigger action.sh on general shells in case that the Action button is unavailable.

--- module/src/action.sh
+++ module/src/action.sh
@@ -1,9 +1,8 @@
-
 printf "Status of NeoZygisk\n\n"
 
 cat @WORK_DIRECTORY@/module.prop
 
-if [ -z $MMRL ] && { [ $KSU = true ] || [ $APATCH = true ]; }; then
+if [[ -z $MMRL ]] && ([[ -n $KSU ]] || [[ -n $APATCH ]]); then
 	# Avoid instant exit on KSU or APATCH
 	sleep 10
 fi
